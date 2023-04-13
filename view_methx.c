4f00b260c2440ae5ff1e8477eeb78f73b8ad27b5
Robin Cristofari <cristofa@puhti-login14.bullx>
2023-04-13 10:35:36 +0300

    WGBS cleanup

--- view_methx.c
+++ view_methx.c
@@ -8,49 +8,74 @@ int main(int argc, char *argv[]) {
 
     int opt; // options in the argument parser
 
-    FILE *ifile;
+    FILE *ifile, *gfile; // input file, and optional genome coordinate file
     int includes = 0, excludes = 0, n_samples=0;
     char keep_string[1024]; // unlikely to be exceeded
-    int filesize, n_rows, n_items = 0;
+    int filesize = 0, n_rows = 0, n_items = 0;
     int array_size = 1000;
     int array_index = 0;
-    int pos;
-    char flag;
-    int n_samples_kept;
+    int pos = 0; // the current position
+    int range_start = 0, range_end = 0; // start and end positions if we extract from a range
+    int abs_start = 0;
+    char chrom[64]; // the chromosome name if we extract from a range
+    char flag = 0;
+    int n_samples_kept = 0;
     int sample_order[512];
-    int i, m, index = 0;
+    int index = 0;
     float *data, *new_data; // pointer to the data array
-    bool do_subset = false;
+    bool do_subset = false, do_range = false;
     char *sp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read = 0;
+
+    printf("+--------------------------------------------------------------------------------\n");
 
     // Command line argument parsing
-    while ((opt = getopt(argc, argv, "i:s:n:x:k:")) != -1) {
+    while ((opt = getopt(argc, argv, "m:s:n:i:x:k:r:f:")) != -1) {
         switch (opt) {
-            case 'i':
+            case 'm':
+                // Input matrix file
                 ifile = fopen(optarg, "r");
-                printf("Input file : %s\n", optarg);
+                printf("+ Input file : %s\n", optarg);
                 break;
             case 's':
+                // Total number of Samples in the matrix file
                 n_samples = atoi(optarg);
-                printf("Matrix file contains %d samples\n", n_samples);
                 break;
-            case 'n':
+            case 'i':
+                // Including types of positions (in binary code format)
                 includes = atoi(optarg);
-                printf("Including %d\n", includes);
                 break;
             case 'x':
+                // eXcluding types of positions (in binary code format)
                 excludes = atoi(optarg);
-                printf("Excluding %d\n", excludes);
                 break;
             case 'k':
+                // We Keep only a list of samples (comma-separated)
                 strcpy(keep_string, optarg);
                 do_subset = true;
                 n_samples_kept = (strlen(optarg)+1)/2;
-                printf("Number of individuals to retain: %d\n", (strlen(optarg)+1)/2);
-                printf("Keeping individuals %s\n", keep_string);
+                //printf("+ Number of individuals to retain: %d\n", (strlen(optarg)+1)/2);
+                printf("+ Keeping %d individuals: %s\n", (strlen(optarg)+1)/2, keep_string);
+                break;
+            case 'r':
+                // We are only exporting data for a particular range. This is provided as a standard string: chrom:start-end.
+                do_range = true;
+                sp = strtok(optarg, ":-");
+                strcpy(chrom, sp);
+                sp = strtok(NULL, ":-");
+                range_start = atoi(sp);
+                sp = strtok(NULL, ":-");
+                range_end = atoi(sp);
+                break;
+            case 'f':
+                // In case we want to export a range, we need a genome index file
+                gfile = fopen(optarg, "r");
+                printf("+ Genome index file: %s\n", optarg);
                 break;
             default:
-                fprintf(stderr, "Usage: %s -i input_matrix -s number_of_samples [-n include_flag] [-x exclude_flag]\n", argv[0]);
+                fprintf(stderr, "Usage: %s -m input matrix -s number of samples [-i include flag] [-x exclude flag] [-k keep list] [-r genomic range] [-f fai index]\n", argv[0]);
                 exit(EXIT_FAILURE);
         }
     }
@@ -67,23 +92,56 @@ int main(int argc, char *argv[]) {
         exit(0);
     }
 
-
-    // Handling the subsetting: if -k is set, parse, otherwise make a list for all samples
+    // Handling the subsetting option:
+    // if -k is set, parse, otherwise make a list for all samples
     if (do_subset == true){
         sp = strtok(keep_string, ",");
         sample_order[0] = atoi(sp);
-        for (i = 1; i < n_samples_kept; i++) {
+        for (int i = 1; i < n_samples_kept; i++) {
             sp = strtok(NULL, ",");
-            printf("%d\n", sp);
+            //printf("%d\n", sp);
             sample_order[i] = atoi(sp);
         }
+        printf("\n");
     } else {
         n_samples_kept = n_samples;
-        for (i=0; i<n_samples; i++){
-            printf("%d ", i);
+        for (int i = 0; i<n_samples; i++){
+            //printf("%d ", i);
             sample_order[i] = i;
         }
-        printf("\n");
+    }
+
+    // Handling of the ranging options:
+    // if -r is set, check that the genome index is given and that the range is parsed correctly
+    if (do_range == true){
+        if (gfile == NULL) {
+            printf("Could not open the genome index file.\nTerminating.\n");
+            exit(0);
+        } else {
+            // Parse the index file until the target scaffold is found:
+
+            while ((read = getline(&line, &len, gfile)) != -1) {
+                //printf("Retrieved line of length %zu:\n", read);
+                //printf("%s", line);
+                sp = strtok(line, "\t");
+                //printf("First chunk %s\n", sp);
+                if (strcmp(sp, chrom) == 0) {
+                    //printf("%s == %s\n", chrom, line);
+                    range_start += abs_start;
+                    range_end += abs_start;
+                    //printf("Absolute range: %d to %d\n", range_start, range_end);
+                    break;
+                }
+                sp = strtok(NULL, "\t");
+                abs_start += atoi(sp);
+            }
+            fclose(gfile);
+
+        }
+        //printf("Absolute start: %d\n", abs_start); // USE THIS WITH RANGE COORDS TO TEST THE POSITIONS FOR OUTPUTTING. WHEN REACHING END, BREAK.
+    } else {
+        // If we are not selecting a range, we set the range to the full file
+        range_end = 2147483647; // max int
     }
 
     // Find out he number of lines in the file
@@ -92,11 +150,20 @@ int main(int argc, char *argv[]) {
     fseek(ifile, 0, SEEK_SET);
     n_rows = filesize / (sizeof(int) + sizeof(char) + sizeof(float) * n_samples);
 
-    printf("Number of records in the input file: %d\n", n_rows);
+    printf("+ Matrix file contains %d samples\n", n_samples);
+    if (do_range == true) {
+        printf("+ Exporting data for scaffold %s at positions %d to %d\n", chrom, range_start-abs_start, range_end-abs_start);
+    } else {
+        printf("+ Exporting data for all available positions\n");
+    }
+    printf("+ Including %d\n", includes);
+    printf("+ Excluding %d\n", excludes);
+    printf("+ Number of records in the methylation matrix: %d\n", n_rows);
+    printf("+--------------------------------------------------------------------------------\n");
 
     data = (float*)malloc(array_size * sizeof(float) * n_samples_kept);
 
-    for (i = 0; i < n_rows; i++){
+    for (int i = 0; i < n_rows; i++){
 
         fread(&pos, sizeof(int), 1, ifile);
         fread(&flag, sizeof(char), 1, ifile);
@@ -104,9 +171,9 @@ int main(int argc, char *argv[]) {
         fread(&meth, sizeof(float), n_samples, ifile);
         //printf("%f\n", meth[0]);
 
-        if (((flag & includes) == includes) && ((flag & excludes) == 0)) {
-            //printf("%d\t", index);
-            for (m = 0; m < n_samples_kept; m++){
+        if (((flag & includes) == includes) && ((flag & excludes) == 0) && (pos >= range_start) && (pos <= range_end)) {
+            printf("%d ", flag);
+            for (int m = 0; m < n_samples_kept; m++){
                 *(data+index) = meth[sample_order[m]];
                 printf("%f ", meth[sample_order[m]]);
                 index++;
@@ -114,12 +181,14 @@ int main(int argc, char *argv[]) {
             printf("\n");
             n_items += 1;
             //printf("Here - %d\n", n_items);
+        } else if (pos > range_end) {
+            //printf("Reached final position: %d > %d\n", pos, range_end);
+            break;
         }
 
         if (n_items >= array_size) {
-            //printf("----------Here----------\n");
             array_size *= 2;
-            printf("Array new size: %d\n", array_size);
+            //printf("Array new size: %d\n", array_size);
             new_data = (float*)realloc(data, array_size*sizeof(float)*n_samples_kept);
             //printf("There\n");
             if (new_data == NULL) {
@@ -130,7 +199,7 @@ int main(int argc, char *argv[]) {
         }
 
     }
-    printf("Matching rows: %d out of %d\n", n_items, n_rows);
+    printf("+ Matching rows: %d out of %d\n", n_items, n_rows);
 
 free(data);
 
